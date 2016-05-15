6445402673cd721e8b9e9a8b00279dbed6e2681a
Jorge Carretero <jorge.jcc@gmail.com>
2016-05-15 20:43:09 +0200

    feat(): maqueta inicial listado de tareas, mock de lista de tareas, recuperar lista de tareas del modelo.

--- app/src/users/UserService.js
+++ app/src/users/UserService.js
@@ -15,34 +15,86 @@
   function UserService($q){
     var users = [
       {
-        name: 'Lia Lugo',
+        name: 'Manson',
         avatar: 'svg-1',
-        content: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.'
+        content: 'Me gusta programar, pero me gusta más aun echar horas',
+        tasksToday: [
+          {
+            date: "2016-05-15",
+            title: "maquetar nuevo diseño pantalla X",
+            finished: false,
+            position: 1
+          },
+          {date: "2016-05-15",
+            title: "pedir por email a Charly que me pase resultados de test de seguridad",
+            finished: false,
+            position: 1
+          },
+          {date: "2016-05-15",
+            title: "Publicar nuevo servicio InformaVistoService",
+            finished: false,
+            position: 1
+          }
+        ],
+        tasksHistory: [
+          {
+            date: "2016-05-14",
+            title: "Desarrollar nuevo servicio InformaVistoService",
+            finished: false,
+            position: 1
+          },
+          {date: "2016-05-14",
+            title: "Enviar solicitud de test de seguridad, entorno ya disponible",
+            finished: false,
+            position: 1
+          },
+          {date: "2016-05-13",
+            title: "Revisión requisitos usuario InformaVistoService",
+            finished: false,
+            position: 1
+          }
+        ]
       },
       {
-        name: 'George Duke',
+        name: 'Viscaralascasca',
         avatar: 'svg-2',
-        content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
-      },
-      {
-        name: 'Gener Delosreyes',
-        avatar: 'svg-3',
-        content: "Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney's blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS."
-      },
-      {
-        name: 'Lawrence Ray',
-        avatar: 'svg-4',
-        content: 'Scratch the furniture spit up on light gray carpet instead of adjacent linoleum so eat a plant, kill a hand pelt around the house and up and down stairs chasing phantoms run in circles, or claw drapes. Always hungry pelt around the house and up and down stairs chasing phantoms.'
-      },
-      {
-        name: 'Ernesto Urbina',
-        avatar: 'svg-5',
-        content: 'Webtwo ipsum dolor sit amet, eskobo chumby doostang bebo. Bubbli greplin stypi prezi mzinga heroku wakoopa, shopify airbnb dogster dopplr gooru jumo, reddit plickers edmodo stypi zillow etsy.'
-      },
-      {
-        name: 'Gani Ferrer',
-        avatar: 'svg-6',
-        content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
+        content: 'Suelo jugar al rol sin parar, y a veces hago commits',
+        tasksToday: [
+          {
+            date: "2016-05-15",
+            title: "Programar comportamientos en la pantalla de comunicaciones",
+            finished: false,
+            position: 1
+          },
+          {date: "2016-05-15",
+            title: "Abrir incidencia por dependencia de host en el api cuando consultamos comunicaciones",
+            finished: false,
+            position: 1
+          },
+          {date: "2016-05-15",
+            title: "Reparto de tareas pantalla de comunicaciones",
+            finished: false,
+            position: 1
+          }
+        ],
+        tasksHistory: [
+          {
+            date: "2016-05-13",
+            title: "Reunión de planificación próximo sprint",
+            finished: false,
+            position: 1
+          },
+          {date: "2016-05-12",
+            title: "Incidencia al consultar el número de mensajes sin leer.",
+            finished: false,
+            position: 1
+          },
+          {date: "2016-05-09",
+            title: "Desarrollo front pantalla de comunicaciones",
+            finished: false,
+            position: 1
+          }
+        ]
       }
     ];
 
